9b832b1ceec5da44bd34819466e12f254dcce109
Eazyisreal <eazyisreal192@gmail.com>
2023-01-02 10:20:38 +0100

    Feat: Added custom cursor animations

--- app.js
+++ app.js
@@ -34,3 +34,6 @@ document.addEventListener('mousemove', e =>{
     cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
 })
 
+document.addEventListener('scroll', e =>{
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+})
